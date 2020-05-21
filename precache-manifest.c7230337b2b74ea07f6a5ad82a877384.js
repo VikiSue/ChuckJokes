be6e3a357e4df3c7682203a3e40e333851ac095c
RomanLev4yk <romanlev4yk@ukr.net>
2020-05-21 16:42:31 +0300

    Updates

---
+++ precache-manifest.c7230337b2b74ea07f6a5ad82a877384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d76cb8457a6a4aa5e445ac4c17ab5cc",
+    "url": "/ChuckJokes/index.html"
+  },
+  {
+    "revision": "dcc77b92cb9d4adc261f",
+    "url": "/ChuckJokes/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4c4c97cc028f6580c0a7",
+    "url": "/ChuckJokes/static/css/main.dc15e4f0.chunk.css"
+  },
+  {
+    "revision": "dcc77b92cb9d4adc261f",
+    "url": "/ChuckJokes/static/js/2.4534f95d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ChuckJokes/static/js/2.4534f95d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4c97cc028f6580c0a7",
+    "url": "/ChuckJokes/static/js/main.57027674.chunk.js"
+  },
+  {
+    "revision": "d175d4aad5a5b3f46fff",
+    "url": "/ChuckJokes/static/js/runtime-main.ede2ddc2.js"
+  }
+]);
