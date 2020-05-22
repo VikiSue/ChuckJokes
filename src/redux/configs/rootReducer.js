9de6e78a0cda7dc645087b6a411494eee8373776
RomanLev4yk <romanlev4yk@ukr.net>
2020-05-22 18:14:42 +0300

    text input clear

--- src/redux/configs/rootReducer.js
+++ src/redux/configs/rootReducer.js
@@ -2,9 +2,19 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import onJokesSearch from "../reducers/onJokesSearch";
 import {onFavoriteAdd} from "../reducers/onFavoriteAdd";
+import {SEARCH_SUCCESS} from "../../types";
 
 export const rootReducer = combineReducers({
   searchForm: onJokesSearch,
   favorite: onFavoriteAdd,
-  form: formReducer
+  form: formReducer.plugin({
+    search: (state, action) => {
+      switch (action.type) {
+        case SEARCH_SUCCESS:
+          return undefined;
+        default:
+          return state;
+      }
+    },
+  })
 });
