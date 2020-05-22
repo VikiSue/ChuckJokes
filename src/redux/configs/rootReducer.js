ca1feffa197c007524ef0a6d6f3fb6200baedfc3
RomanLev4yk <romanlev4yk@ukr.net>
2020-05-22 18:21:14 +0300

    fix redux-form

--- src/redux/configs/rootReducer.js
+++ src/redux/configs/rootReducer.js
@@ -2,19 +2,10 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import onJokesSearch from "../reducers/onJokesSearch";
 import {onFavoriteAdd} from "../reducers/onFavoriteAdd";
-import {SEARCH_SUCCESS} from "../../types";
+
 
 export const rootReducer = combineReducers({
   searchForm: onJokesSearch,
   favorite: onFavoriteAdd,
-  form: formReducer.plugin({
-    search: (state, action) => {
-      switch (action.type) {
-        case SEARCH_SUCCESS:
-          return undefined;
-        default:
-          return state;
-      }
-    },
-  })
+  form: formReducer
 });
